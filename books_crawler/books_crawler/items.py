67b00025b003ed5083900b6d6635678bab3f0acf
Kaung Myat Naing <knaing@hawk.iit.edu>
2024-04-21 17:22:57 -0500

    a
    a

--- books_crawler/books_crawler/items.py
+++ books_crawler/books_crawler/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class BooksCrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class BookItem(scrapy.Item):
+    title = scrapy.Field()
+    price = scrapy.Field()
+    rating = scrapy.Field()

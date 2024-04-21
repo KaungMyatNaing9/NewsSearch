3b75306df69ade9f9abad97670243a24abc63f57
Kaung Myat Naing <knaing@hawk.iit.edu>
2024-04-21 16:42:11 -0500

    item
    putting item needed

--- news_crawler/news_crawler/items.py
+++ news_crawler/news_crawler/items.py
@@ -7,6 +7,7 @@ import scrapy
 
 
 class NewsCrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author = scrapy.Field()
+    publication_date = scrapy.Field()
+    content = scrapy.Field()

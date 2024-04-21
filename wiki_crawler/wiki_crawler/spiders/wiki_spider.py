81df4fb8400568f669b5de790e7f81aa77f10cf1
Kaung Myat Naing <knaing@hawk.iit.edu>
2024-04-21 18:44:51 -0500

    1
    1

--- wiki_crawler/wiki_crawler/spiders/wiki_spider.py
+++ wiki_crawler/wiki_crawler/spiders/wiki_spider.py
@@ -3,7 +3,11 @@ import scrapy
 class WikiSpider(scrapy.Spider):
     name = 'wiki'
     allowed_domains = ['en.wikipedia.org']
-    start_urls = ['https://en.wikipedia.org/wiki/Special:Random']
+    start_urls = ['https://en.wikipedia.org/wiki/Wikipedia:Contents/People_and_self',
+                  'https://en.wikipedia.org/wiki/Wikipedia:Contents/Technology_and_applied_sciences',
+                  'https://en.wikipedia.org/wiki/Wikipedia:Good_articles',
+                  'https://en.wikipedia.org/wiki/Wikipedia:Contents/Natural_and_physical_sciences',
+                  'https://en.wikipedia.org/wiki/Wikipedia:Contents/History_and_events']
 
     custom_settings = {
         'DEPTH_LIMIT': 3,
